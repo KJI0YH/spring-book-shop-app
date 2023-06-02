4ec3bcf479fdecd0405cd55faa852bab1acfff9f
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-06-02 15:34:21 +0300

    task 1: connect genres page

--- src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
+++ src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 @RequestMapping("/bookshop")
 public class MainPageController {
