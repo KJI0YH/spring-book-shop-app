dd63305ea7c3282ac6134dd30f17109fe2b9694e
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-06-02 18:15:39 +0300

    add authors from db to view

--- src/main/java/com/example/mybookshopapp/services/BookService.java
+++ src/main/java/com/example/mybookshopapp/services/BookService.java
@@ -1,5 +1,6 @@
-package com.example.mybookshopapp.data;
+package com.example.mybookshopapp.services;
 
+import com.example.mybookshopapp.dto.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
