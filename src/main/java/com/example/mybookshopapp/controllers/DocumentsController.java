58a82a8072dd8923ac3bb387bf4e8c449747edf1
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-06-04 14:44:55 +0300

    add documents controller

---
+++ src/main/java/com/example/mybookshopapp/controllers/DocumentsController.java
@@ -0,0 +1,15 @@
+package com.example.mybookshopapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/documents")
+public class DocumentsController {
+
+    @GetMapping
+    public String documentPage(){
+        return "/documents/index";
+    }
+}
