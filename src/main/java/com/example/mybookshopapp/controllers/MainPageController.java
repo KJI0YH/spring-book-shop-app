0492ae87e712561ed79673f80151066544562936
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-06-10 05:05:42 +0300

    add recent books pagination

--- src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
+++ src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -26,46 +23,45 @@ public class MainPageController {
     }
 
     @ModelAttribute("recommendedBooks")
-    public List<BookEntity> recommendedBooks(){
+    public List<BookEntity> recommendedBooks() {
         return bookService.getPageOfRecommendedBooks(0, 6).getContent();
     }
 
     @ModelAttribute("recentBooks")
-    public List<BookEntity> recentBooks(){
+    public List<BookEntity> recentBooks() {
         return bookService.getPageOfRecentBooks(null, null, 0, 6).getContent();
     }
 
     @ModelAttribute("popularBooks")
-    public List<BookEntity> popularBooks(){
+    public List<BookEntity> popularBooks() {
         return bookService.getPageOfPopularBooks(0, 6).getContent();
     }
 
     @ModelAttribute("searchWordDto")
-    public SearchWordDto searchWordDto(){
+    public SearchWordDto searchWordDto() {
         return new SearchWordDto();
     }
 
     @ModelAttribute("searchResults")
-    public List<BookEntity> searchResults(){
+    public List<BookEntity> searchResults() {
         return new ArrayList<>();
     }
 
     @GetMapping("/")
-    public String mainPage(Model model){
+    public String mainPage(Model model) {
         return "index";
     }
 
     @GetMapping(value = {"/search", "/search/{searchWord}"})
-    public String getSearchResults(@PathVariable(value = "searchWord", required = false) SearchWordDto searchWordDto, Model model){
+    public String getSearchResults(@PathVariable(value = "searchWord", required = false) SearchWordDto searchWordDto, Model model) {
         model.addAttribute("searchWordDto", searchWordDto);
-        model.addAttribute("searchResults", bookService.getPageOfSearchResultBooks(searchWordDto.getExample(), 0, 5).getContent());
+        model.addAttribute("searchResults", bookService.getPageOfSearchResultBooks(searchWordDto.getExample(), 0, 20).getContent());
         return "/search/index";
     }
 
     @GetMapping("/search/page/{searchWord}")
     @ResponseBody
-    public BooksPageDto getNextSearchPage(@RequestParam("offset")Integer offset, @RequestParam("limit")Integer limit,
-                                          @PathVariable(value = "searchWord", required = false)SearchWordDto searchWordDto){
+    public BooksPageDto getNextSearchPage(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit, @PathVariable(value = "searchWord", required = false) SearchWordDto searchWordDto) {
         return new BooksPageDto(bookService.getPageOfSearchResultBooks(searchWordDto.getExample(), offset, limit).getContent());
     }
 }
