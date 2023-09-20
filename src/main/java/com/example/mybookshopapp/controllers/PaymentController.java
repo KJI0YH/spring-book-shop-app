f24ee4a98b75d3027c446a3d64ab07cc966e18bc
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-09-20 11:20:30 +0300

    add cross origin link

--- src/main/java/com/example/mybookshopapp/controllers/PaymentController.java
+++ src/main/java/com/example/mybookshopapp/controllers/PaymentController.java
@@ -15,10 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
@@ -45,6 +42,7 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
+    @CrossOrigin(origins = "https://yoomoney.ru")
     public ResponseEntity<?> handlePayment(@RequestBody PaymentDto payment) throws URISyntaxException, IOException, InterruptedException {
 
         UserEntity user = (UserEntity) userRegister.getCurrentUser();
