51b378198363eccff8e1c058da520f78336f28cc
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-10-05 10:11:17 +0300

    add book popularity update with viewed books

--- src/main/resources/db/migration/V7_0_0__book2user_viewed.sql
+++ src/main/resources/db/migration/V7_0_0__book2user_viewed.sql
@@ -13,3 +13,52 @@ CREATE TABLE IF NOT EXISTS book2user_viewed
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+-- Trigger function to update popularity when inserting to book2user_viewed
+CREATE OR REPLACE FUNCTION update_popularity_insert_book2user_viewed()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE book
+    SET popularity = popularity + 1
+    WHERE id = NEW.book_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger function to update popularity when deleting from book2user_viewed
+CREATE OR REPLACE FUNCTION update_popularity_delete_book2user_viewed()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE book
+    SET popularity = popularity - 1
+    WHERE id = NEW.book_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE delete_old_book2user_views()
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM book2user_viewed
+    WHERE time < now() - interval '1 week';
+END;
+$$;
+
+-- Trigger to update popularity when inserting
+CREATE TRIGGER update_popularity_insert_book2user_viewed_trigger
+    AFTER INSERT
+    ON book2user_viewed
+    FOR EACH ROW
+EXECUTE FUNCTION update_popularity_insert_book2user_viewed();
+
+-- Trigger to update popularity when deleting
+CREATE TRIGGER update_popularity_delete_book2user_viewed_trigger
+    AFTER DELETE
+    ON book2user_viewed
+    FOR EACH ROW
+EXECUTE FUNCTION update_popularity_delete_book2user_viewed();
